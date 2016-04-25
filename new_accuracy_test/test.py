73fc2a7e026ff91d4a14538bb803f04f3d91fd9c
Guo Xiaoyang <guoxy95@hotmail.com>
2016-04-25 21:47:55 +0800

    fix bug test.py

--- new_accuracy_test/test.py
+++ new_accuracy_test/test.py
@@ -10,8 +10,8 @@ fpkls = ["../pkl/full_test_rgb_original_no_mirror_no_crop_seg25.pkl", \
 score_weights = [[1, 2], [1, 1], [2, 1], [1, 3], [3, 1]]
 combine_score(tags, fpkls, score_weights)
 
-tags = ['rgb_original', "conv1_1minus_rgb5"]
-fpkls = ["../pkl/full_test_rgb_original_no_mirror_no_crop_seg25.pkl", \
+tags = ['rgb_original_mc', "conv1_1minus_rgb5"]
+fpkls = ["../pkl/full_test_rgb_fc8_mirror_crop_seg25.pkl", \
 	"../pkl/full_test_conv1_1minus_rgb5_initfromvgg_iter30000_mirror_crop_seg25.pkl"]
 score_weights = [[1, 2], [1, 1], [2, 1], [1, 3], [3, 1]]
 combine_score(tags, fpkls, score_weights)
