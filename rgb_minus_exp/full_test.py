94d0e935e0ee63a154c80cca6b363d477f52cc28
Guo Xiaoyang <guoxy95@hotmail.com>
2016-04-25 19:35:33 +0800

    clean

--- rgb_minus_exp/full_test.py
+++ rgb_minus_exp/full_test.py
@@ -1,63 +1,16 @@
-import numpy as np
-import caffe
-import sys
-import cPickle as pickle
+from ../full_test import full_test
 
-caffe.set_device(3)
-caffe.set_mode_gpu()
-num_videos = 3783
-# num_videos = 20
 
-# model to be tested
 caffemodel = './cuhk_action_recognition_vgg_16_conv1minus_rgb5_initfromvgg_split1_rgb_iter_20000.caffemodel'
 net_prototxt = 'conv1minus_rgb5/train_val_fast.prototxt'
 pkl_dir = "conv1minus_rgb5/"
 experiment_name = "conv1minus_rgb5_initfromvgg_iter20000"
-output_layer_name = "fc8"
+output_layer_names = ["fc8"]
+device_id = 3
 
-# parameters to set
 do_mirror = False
 do_crop = False
 test_segments = 1
 test_batch_size = 1 
 
-# generate test_param_tag
-test_param_tag = ("mirror" if do_mirror else "no_mirror") + ("_crop" if do_crop else "_no_crop") + "_seg" + str(test_segments)
-pickle_file = 'full_test_' + experiment_name + '_' + test_param_tag + '.pkl'
-samples_per_video = (2 if do_mirror else 1) * (5 if do_crop else 1) * test_segments
-assert samples_per_video % test_batch_size == 0
-print("test_param_tag is {}".format(test_param_tag))
-print("{} data will be written to {}".format(output_layer_name, pickle_file))
-print("samples_per_video is {}".format(samples_per_video))
-
-net = caffe.Net(net_prototxt, caffemodel, caffe.TEST)
-num_correct = 0
-
-all_output = []
-for video_id in range(num_videos):
-	video_output = np.zeros([1 * samples_per_video, 101])
-	label = -1;
-	for offset in range(0, samples_per_video, test_batch_size):	
-		net.forward()
-		batch_output = net.blobs[output_layer_name].data
-		video_output[offset: offset+test_batch_size, :] = batch_output
-		if offset == 0:
-			label = int(net.blobs['label'].data[0,0,0])
-		else:
-			assert label == int(net.blobs['label'].data[0,0,0])
-	pd_label = np.argmax(video_output.mean(axis=0))
-	all_output.append((video_id, video_output, label))
-	if pd_label == label:
-		num_correct += 1
-	print("video_id: {}".format(video_id))
-	print("{}".format(np.argmax(video_output, axis=1)))
-	print("result: {}, {}".format(pd_label, label))
-	print("accuracy: {}/{} {}".format(num_correct, video_id+1, num_correct/float(video_id + 1)))
-
-print("accuracy: {}/{} {}".format(num_correct, num_videos, num_correct/float(num_videos)))
-
-with open(pkl_dir+pickle_file, "wb") as f:
-	pickle.dump(all_output, f)
-
-print("pkl file is written to {}".format(pickle_file))
-
+full_test(caffemodel, net_prototxt, pkl_dir, experiment_name, output_layer_names, device_id, do_mirror, do_crop, test_segments, test_batch_size)
