0173a385fa86f5d7f35ef124df0744d79ebe4711
Guo Xiaoyang <guoxy95@hotmail.com>
2016-04-26 00:38:19 +0800

    update train flow

--- flow_1img/train.sh
+++
@@ -1,2 +0,0 @@
-#highdrop exp
-caffe train -solver ./solver.prototxt -weights ./flow1_init.caffemodel -gpu 1 | tee log_train_flow1.txt
---
+++ train_flow/flow_1img/full_test.py
@@ -0,0 +1,18 @@
+import sys
+from full_test import full_test
+sys.path.insert(0, '../../')
+
+device_id = 2
+
+caffemodel = './flow_1img_iter_15000.caffemodel'
+net_prototxt = './train_val_fast.prototxt'
+pkl_dir = "./"
+experiment_name = "flow_1img_iter15000"
+output_layer_names = ["flow_fc8"]
+
+do_mirror = True
+do_crop = True
+test_segments = 25
+test_batch_size = 5
+
+full_test(caffemodel, net_prototxt, pkl_dir, experiment_name, output_layer_names, device_id, do_mirror, do_crop, test_segments, test_batch_size)
