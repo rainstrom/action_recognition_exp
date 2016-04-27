f5536c7cd313b456abb4a5b0310aa4c0b8e5fa8a
Xiaoyang Guo <guoxy95@163.com>
2016-04-27 19:21:51 +0800

    flow_10img for high lr for fc

--- train_flow/flow_10img/train.sh
+++ train_flow/flow_10img/train.sh
@@ -1,2 +1,3 @@
 # train all, init_lr 0.001, step 10000, gamma: 0.1, max_iter: 30000, batch 100
-caffe train -solver ./solver.prototxt -weights ./flow_10img_init.caffemodel -gpu 1 | tee log_train_flow_10img.txt
+# fc for high lr 5x for fc6 and 10x for fc7,8
+caffe train -solver ./solver.prototxt -weights ./flow_10img_init.caffemodel -gpu 6 | tee log_train_flow_10img.txt
