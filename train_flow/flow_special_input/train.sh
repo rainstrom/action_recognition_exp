8eed97da47def8b363667fa9ab04b31c1c55650d
Xiaoyang Guo <guoxy95@163.com>
2016-04-28 21:13:31 +0800

    flow special input update

--- train_flow/flow_special_input/train.sh
+++ train_flow/flow_special_input/train.sh
@@ -1,2 +1,5 @@
 # train all, init_lr 0.001, step 10000, gamma: 0.1, max_iter: 30000, batch 100
-caffe train -solver ./solver.prototxt -weights ./flow_10img_init.caffemodel -gpu 1 | tee log_train_flow_1mean_9minus.txt
+# fc6 *5, fc7,8 *10 lr
+# stop at flow_special_iter_17817.caffemodel
+# restore fc lr
+caffe train -solver ./solver.prototxt -weights ./flow_10img_init.caffemodel -gpu 4 | tee log_train_flow_special_input.txt
